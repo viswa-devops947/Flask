c4d156a620eec2c53c3aff9fa6f0cb9aafb3ecb7
vmarukur <vmarukur@cisco.com>
2018-11-04 13:34:10 -0800

    dynamic routes flask app

---
+++ 02_dynamic_routes/runner.py
@@ -0,0 +1,18 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+	return '<h1>Hello World!</h1>'
+
+@app.route('/information')
+def page_info():
+	return '<h1>This page gives your information</h2>'
+
+@app.route('/user/<name>')
+def user_info(name):
+	return '<h1>Welcome {}, to the user page</h1>'.format(name)
+
+if __name__ == '__main__':
+	app.run()
